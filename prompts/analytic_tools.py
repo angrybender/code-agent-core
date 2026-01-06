92db0e29d9aa1edb8613d5cbb50b9b6a7f2644a4
Kir <dev.temp.null.mail@gmail.com>
2026-01-06 08:37:29 +0400

    fix prompt

--- prompts/analytic_tools.py
+++ prompts/analytic_tools.py
@@ -44,7 +44,7 @@ tools = [
                 "properties": {
                     "text": {
                         "type": "string",
-                        "description": "Text is a report in the markdowm format. Dont write full content of files - short description is enough!"
+                        "description": "Text is a report in the markdown format. Dont write full content of files - short description is enough!"
                     }
                 }
             }
